837a6170a1bb86b89e45b27c492ae3dd2056ea10
Dylan Mann <dylanm2094@gmail.com>
2021-02-01 21:16:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function happyHolidays(){
   return "Happy holidays!"
-}// write your code below!
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days,holiday){
+  return `It's ${days} days until ${holiday}!`
+}
